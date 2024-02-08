de6fa2190d682d2aaf4729538a2a09ea7790cb88
Mutava-Ndinda <ndindafmutava@gmail.com>
2024-02-08 21:44:13 +0300

    kotlin 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name = "Faith Mutava"
+    println(name)
+    var age =21
+    println(age)
+    var contact = "0112441038"
+    println(contact)
+    var weight: Float = 46.6F
+    println(weight)
+    var kenyanCitizen: Boolean =true
+    println(kenyanCitizen)
+    var nonkenyanCitizen: Boolean = false
+    println(nonkenyanCitizen)
+
+}
